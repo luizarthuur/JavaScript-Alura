8599591ffbebdc831b9d1335bec2fd349f313153
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-04-07 22:58:44 -0300

    início do curso!

---
+++ logica-js-projeto_inicial/Exercicios/exercicios.js
@@ -0,0 +1,23 @@
+alert("Boas vindas ao nosso site!");
+
+let nome =  "Lua";
+
+let idade = 25;
+
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;
+
+alert("Erro, você deve preencher todos os campos!");
+
+let mensagemDeErro = "Erro, você deve preencher todos os campos!";
+
+alert(mensagemDeErro);
+
+let nome2 = prompt("Digite o seu nome");
+
+let idade2 = prompt("Digite a sua idade:")
+
+if(idade2 > 18) {
+    alert("Você já pode tirar a habilitação!");
+}
