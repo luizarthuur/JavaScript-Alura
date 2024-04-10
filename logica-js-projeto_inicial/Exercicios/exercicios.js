c51e97d5d023c3e41a41e398dc9dcc32750674cc
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-04-09 22:59:32 -0300

    Update do jogo e criação de alguns exercicios

--- logica-js-projeto_inicial/Exercicios/exercicios.js
+++ logica-js-projeto_inicial/Exercicios/exercicios.js
@@ -1,23 +1,74 @@
-alert("Boas vindas ao nosso site!");
+//alert("Boas vindas ao nosso site!");
 
-let nome =  "Lua";
+//let nome =  "Lua";
 
-let idade = 25;
+//let idade = 25;
 
-let numeroDeVendas = 50;
+//let numeroDeVendas = 50;
 
-let saldoDisponivel = 1000;
+//let saldoDisponivel = 1000;
 
-alert("Erro, você deve preencher todos os campos!");
+//alert("Erro, você deve preencher todos os campos!");
 
-let mensagemDeErro = "Erro, você deve preencher todos os campos!";
+//let mensagemDeErro = "Erro, você deve preencher todos os campos!";
 
-alert(mensagemDeErro);
+//alert(mensagemDeErro);
 
-let nome2 = prompt("Digite o seu nome");
+//let nome2 = prompt("Digite o seu nome");
 
-let idade2 = prompt("Digite a sua idade:")
+//let idade2 = prompt("Digite a sua idade:");
 
-if(idade2 > 18) {
-    alert("Você já pode tirar a habilitação!");
-}
+//if(idade2 > 18) {
+//    alert("Você já pode tirar a habilitação!");
+//}
+
+//alert("Bem vindo ao programa do fim de semana!");
+
+//let diaDaSemana = prompt("Que dia da semana é hoje?");
+
+//if (diaDaSemana == "Sábado" || diaDaSemana == "Domingo"); {
+//    alert("Que legal, hoje é fim de semana!");
+//}
+
+//else{
+//    alert("Obrigado, te desejo uma boa semana!");
+//}
+
+//alert("Bem vindo ao programa verificador de números!");
+
+//let numero = prompt("Qual é o número que você escolheu?");
+
+//if (numero > 0) {
+//    alert("O seu número é positivo!");
+//}
+
+//else if(numero == 0) {
+//    alert("O seu número é o 0 ele não é negativo nem positivo");
+//}
+
+//else{
+    
+//    alert("O seu número é negativo");
+//}
+
+//alert("Programa da pontuação");
+
+//let pontuacao = prompt("Qual foi a sua pontuação?");
+
+//if (pontuacao >= 100) {
+//    alert("Parabéns, você venceu!");
+//}
+
+//else{
+//    alert("Tente novamente para ganhar");
+//}
+
+//alert("Bem vindo a sua conta!");
+
+//let valorDaConta = 6000;
+
+//alert(`O valor da sua conta é R$ ${valorDaConta}`);
+
+let nome = prompt("Olá, qual é o seu nome?");
+
+alert(`Seja muito bem vindo! ${nome}!`);
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,8 +1,19 @@
 alert("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 29;
-let chute = prompt("Escolha um número entre 1 e 30");
+console.log(numeroSecreto);
 
+//declarando o chute e também informando ao desenvolvedor
+let chute = prompt("Escolha um número entre 1 e 30");
+console.log(chute);
+console.log('Resultado da comparação', chute == numeroSecreto);
+
+
+// se chute for igual ao número secreto
 
 if(numeroSecreto == chute) {
-    console.log("Isso aí, você descobriu o número secreto (29)");
+    alert(`Isso aí, você descobriu o número secreto (${numeroSecreto})`);
+}
+
+else{
+    alert("Você errou!");
 }
